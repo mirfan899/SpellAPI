1efcc40e00c47d08aa7029b267ece7337f2f9de5
Muhammad Irfan <virtuoso.irfan@gmail.com>
2022-08-19 21:35:36 +0500

    bigram on the fly.

--- bigram.py
+++ bigram.py
@@ -6,20 +6,21 @@ from gensim.models.phrases import Phrases, ENGLISH_CONNECTOR_WORDS
 nlp = English()
 tokenizer = Tokenizer(nlp.vocab)
 
-sentences = []
-bigram = Phrases()
-with open("corpus.txt", "r") as ifile:
+bigram = Phrases(delimiter=" ",  min_count=5, threshold=10)
+with open("en_wiki.txt", "r") as ifile:
     for line in ifile:
         sentence = [word.text for word in tokenizer(line.strip())]
-        sentences.append(sentence)
         bigram.add_vocab([sentence])
 
-
-bigram_model = Phrases(sentences, min_count=2, threshold=2, connector_words=ENGLISH_CONNECTOR_WORDS, delimiter=" ")
 bigram_model_counter = Counter()
-for key in bigram_model.vocab.keys():
+for key in bigram.vocab.keys():
     if len(key.split(" ")) > 1:
-        bigram_model_counter[key] += bigram_model.vocab[key]
+        bigram_model_counter[key] += bigram.vocab[key]
 
-for key, counts in bigram_model_counter.most_common(50):
-    print('{} {}'.format(key, counts))
+# for key, counts in bigram_model_counter.most_common(50):
+#     print('{} {}'.format(key, counts))
+
+with open("frequency_bigramdictionary.txt", "w") as writer:
+    for key, counts in bigram_model_counter.items():
+        if counts >= 10:
+            writer.write('{} {}\n'.format(key, counts))
